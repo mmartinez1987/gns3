c24ab312a36cad281834bfbd32c6ed9c317647ef
Michael <michaeldm.martinez@gmail.com>
2018-03-18 20:45:21 -0600

    Update

--- open_terminals.py
+++ open_terminals.py
@@ -1,39 +1,29 @@
 import socket
-import sys
-import os
 import subprocess
 
-class openterminals():
+def portscan():
+	ports = []
+	localhost='127.0.0.1'
+	counter = 0
+	while (counter < 100):
+		for port in range(2000,3000):
+			counter += 1
+			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			result = sock.connect_ex((localhost,port))
+			if result == 0:
+				print('Host found adding port %s to ports array' % port)
+				ports.append(port)
+			sock.close()
+	return ports
 
-    global ports
-    ports = []
+def open_terminals(ports = []):
+	for x in range(0,len(ports)):
+		print('Opening terminal to router on port %s' % ports[x])
+		subprocess.Popen(["xterm", "-e", "telnet localhost %s" % ports[x]])
 
-    def portscan():
-          localhost='127.0.0.1'
-          counter = 0
-          print('running createrouters()') 
-        while (counter < 100):
-            try:
-              for port in range(5000,5100):
-            counter += 1
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            result = sock.connect_ex((localhost, port))
-            if result == 0:
-              print('hostfound adding port')
-                   ports.append(port)
-        sock.close()
+def main():
+	ports = portscan()
+	open_terminals(ports)
 
-            except ValueError:
-              print('Failure')
-
-      
-      nhosts = len(ports) #set nhosts to size of list to be used in for loop 
-      index = len(ports)  #set index to size of list to be used to call port numbers in ports
-
-    #run for loop and open new terminal for each router
-      for x in range(0, nhosts):
-            subprocess.Popen(["xterm", "-e", "telnet localhost %s" % ports[index - 1]])
-          index -= 1
-
-
-        portscan()
+if __name__ == '__main__':
+	main()
